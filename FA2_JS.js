26c35f62038bbfb23bb02d9d92e0c62352b82738
JessNoelHAbanilla <jessnoel.abanilla@smc.pshs.edu.ph>
2025-02-08 21:45:03 +0800

    Update FA2_JS.js
    edited js file

--- FA2_JS.js
+++ FA2_JS.js
@@ -1,17 +1,14 @@
 function Calculate_sum()
 {
 	var num1 = document.getElementById("num1").value;
-	document.getElementById("num1").innerHTML = num1;
 	var num2 = document.getElementById("num2").value;
-	document.getElementById("num2").innerHTML = num2;
 	var sum = parseInt(num1) + parseInt(num2);
 	document.getElementById("result").innerHTML = ("The sum of " + num1 + " and " + num2 + " is " + sum + ".");
 	return sum;
 }
 function Calculate_diff()
 {
-    var num1 = document.getElementById("num1").value;
-	document.getElementById("num1").innerHTML = num1;
+        var num1 = document.getElementById("num1").value;
 	var num2 = document.getElementById("num2").value;
 	var diff = num1 - num2;
 	document.getElementById("result").innerHTML = ("The difference of " + num1 + " and " + num2 + " is " + diff + ".");
@@ -19,8 +16,7 @@ function Calculate_diff()
 }
 function Calculate_prod()
 {
-    var num1 = document.getElementById("num1").value;
-	document.getElementById("num1").innerHTML = num1;
+        var num1 = document.getElementById("num1").value;
 	var num2 = document.getElementById("num2").value;
 	var prod = num1 * num2;
 	document.getElementById("result").innerHTML = ("The product of " + num1 + " and " + num2 + " is " + prod + ".");
@@ -28,8 +24,7 @@ function Calculate_prod()
 }
 function Calculate_quo()
 {
-    var num1 = document.getElementById("num1").value;
-	document.getElementById("num1").innerHTML = num1;
+        var num1 = document.getElementById("num1").value;
 	var num2 = document.getElementById("num2").value;
 	var quo = num1 / num2;
 	document.getElementById("result").innerHTML = ("The quotient of " + num1 + " and " + num2 + " is " + quo + ".");
@@ -37,8 +32,7 @@ function Calculate_quo()
 }
 function Calculate_rem()
 {
-    var num1 = document.getElementById("num1").value;
-	document.getElementById("num1").innerHTML = num1;
+        var num1 = document.getElementById("num1").value;
 	var num2 = document.getElementById("num2").value;
 	var rem = num1 % num2;
 	document.getElementById("result").innerHTML = ("The remainder of " + num1 + " and " + num2 + " is " + rem + ".");
